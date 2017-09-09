94c111d98a1ef627e2748e5eef56f336768fb8ac
Robert Laws <roblaws@me.com>
2017-09-09 15:38:48 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,4 +24,11 @@ class Transfer
       end
     end
   end
+
+  def reverse_transfer
+    if @status == "complete"
+      sender.balance += amount
+      receiver.balance -= amount
+    end
+  end
 end
